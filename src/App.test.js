ed56964c75cdf06da3c8c4524a00b1655d4ac08a
gabrielaelaela <ticugabriela12@gmail.com>
2022-10-30 14:36:24 +0300

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from'@testing-library/react'
 import App from './App';
 
-test('renders learn react link', () => {
+/*test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
-});
+});*/
+
+
+test('add todo', () => {
+  render(<App />);
+  const desc = screen.getByPlaceholderText('Description');
+  fireEvent.change(desc, { target: { value: 'Go to coffee' } });
+  const date = screen.getByPlaceholderText('Date');
+  fireEvent.change(date, { target: { value: '29.01.2021' } });
+  const button = screen.getByText('Add');
+  fireEvent.click(button);
+  const tablecell = screen.getByText(/go to coffee/i);
+  expect(tablecell).toBeInTheDocument();
+})
